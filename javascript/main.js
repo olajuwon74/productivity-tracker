fc4fd51fbefe9d8535b74a60d7d91af3f2ce9ff1
Solomon <adebayosolomon74@gmail.com>
2021-11-13 11:28:12 +0100

    added popups

--- javascript/main.js
+++ javascript/main.js
@@ -15,3 +15,14 @@
 //     navbar.classList.remove("sticky");
 //   }
 // }
+
+var modal = null
+ function pop() {
+   if(modal === null) {
+     document.getElementById("box").style.display = "block";
+     modal = true
+   } else {
+     document.getElementById("box").style.display = "none";
+     modal = null
+   }
+ }
