190c4d3ed113adfcce703537be5d9c7001fb8165
sorxcode <sorxcode@gmail.com>
2021-11-14 07:34:40 +0100

    added history view

--- app.py
+++ app.py
@@ -1,23 +1,42 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
+import script
 
 TEMPLATE_DIR = 'assets'
 STATIC_DIR = 'assets'
 app = Flask(__name__, template_folder=TEMPLATE_DIR, static_folder=STATIC_DIR)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
+
 @app.route('/view')
-def view():
+def view(start_date=None, end_date=None):
     """
     Loads the view page
     """
-    return render_template('html/view.html')
+    header = script.get_header()
+    next_line = {}
+
+    if not (start_date and end_date):
+        next_line = script.get_next_line()
+        # exclude the last line from the list, since it is the next_line variable
+        lines_history = script.get_history()[:-1]
+    else:
+        lines_history = script.get_history(start_date, end_date)
+
+    return render_template('html/view.html',
+                           header=header,
+                           next_line=next_line,
+                           history=lines_history)
+
 
 @app.route('/history')
 def history():
     """
     Loads the history page
     """
-    return render_template('html/history.html')
+    daily_history = script.get_summarized_daily_history()
+    print(daily_history)
+    return render_template('html/history.html', daily_history=daily_history)
+
 
 @app.route('/productivity')
 def productivity():
@@ -25,6 +44,7 @@ def productivity():
     Loads the productivity page
     """
     return render_template('html/productivity.html')
+
 
 @app.route("/")
 def home():
--- script.py
+++ script.py
@@ -1,14 +1,92 @@
-# import csv
+import csv
+from collections import Counter
+from datetime import date, timedelta
 
-# csv_file = open('records/sim_df.csv', 'r')
-# csv_reader = csv.reader(csv_file)
+csv_file = open('records/sim_df_updated.csv', 'r')
+csv_reader = csv.reader(csv_file)
+
+#  local state variables
+header = next(csv_reader, None)
+header = [col_name.title().replace("_", " ") for col_name in header]
+history = []
 
 
-# header = next(csv_reader, None)
+def get_next_line() -> dict:
+    """
+    Reads the next line from the csv file, save it to history and returns it as a dict.
+    """
+    next_line = next(csv_reader, None)
+
+    if next_line:
+        next_line = dict(zip(header, next_line))
+        history.append(next_line)
+        return next_line
+    return {}
 
 
-# i = 0
-# while i < csv_reader.line_num:
-#     r = next(csv_reader, None)
-#     print(r)
-#     i += 1
+def get_header() -> list:
+    """
+    Returns the header of the csv file.
+    """
+    return header
+
+
+def get_history(start_date=None, end_date=None) -> list:
+    """
+    Returns the history of the csv file.
+    """
+    def get_records_in_range(start_date, end_date):
+        # get the history between the start and end date
+        history_logs = get_daily_history()
+        _history = []
+
+        start_date = date.fromisoformat(start_date)
+        end_date = date.fromisoformat(end_date)
+        while start_date <= end_date:
+            _history.append(history_logs[start_date.isoformat()])
+            start_date += timedelta(days=1)
+        return _history
+
+    if start_date and end_date:
+        return get_records_in_range(start_date, end_date)
+    elif start_date:
+        return get_records_in_range(start_date, date.today().isoformat())
+    elif end_date:
+        return get_records_in_range(date.today().isoformat(), end_date)
+    return history
+
+
+def get_daily_history() -> dict:
+    """
+    Returns the history of the csv file grouped by day.
+    """
+    daily_history = {}
+    for line in history:
+        day = line['Start Timestamp'].split(" ")[0]
+        if day in daily_history:
+            daily_history[day].append(line)
+        else:
+            daily_history[day] = [line]
+    return daily_history
+
+
+def get_summarized_daily_history() -> dict:
+    """
+    Returns the history of the csv file summarized by day.
+    """
+    daily_history = get_daily_history()
+    summarized_history = {}
+
+    for day, records in daily_history.items():
+        sequences = [record.get("Sequence") for record in records]
+        most_common_sequences = [count_pair[0]
+                                 for count_pair in Counter(sequences).most_common(2)]
+
+        # since scoring is realtime, we need to take the last record of the day
+        status = records[-1].get("Cat")
+
+        summarized_history[day] = {
+            "status": status,
+            "sequence": "\n".join(most_common_sequences)
+        }
+    return summarized_history
